5eb8596cb090cb1735a69fcd8ab2a1694c8769e5
isisisisisisithch <dallucus@gmail.com>
2020-11-29 22:12:06 -0500

    review

--- src/ca/bytetube/_00_leetcode/lc_arraylist_01_MergeSortedArray.java
+++ src/ca/bytetube/_00_leetcode/lc_arraylist_01_MergeSortedArray.java
@@ -18,4 +18,67 @@ public class lc_arraylist_01_MergeSortedArray {
         }
 
     }
+
+    public static int[] merge2(int[] nums1, int m, int[] nums2, int n){
+        int l1 = 0;
+        int l2 = 0;
+        int curr = 0;
+        int[] nums3 = new int[m + n];
+        while (l1 <= m - 1 && l2 <= n - 1) {
+            if (nums1[l1] < nums2[l2]) {
+                nums3[curr++] = nums1[l1++];
+            }else {
+                nums3[curr++] = nums2[l2++];
+            }
+        }
+
+        if (l1 > m - 1) {
+            while (l2 <= n - 1) {
+                nums3[curr++] = nums2[l2++];
+            }
+        }else if (l2 > n - 1) {
+            while (l1 <= m - 1) {
+                nums3[curr++] = nums1[l1++];
+            }
+        }
+        return nums3;
+//        int[] nums3 = new int[nums1.length];
+//
+//        int i3 = 0;
+//        int i1 = 0;
+//        int i2 = 0;
+//        while (i1 < m  && i2 < n){
+//            if (nums1[i1] <= nums2[i2]){
+//                nums3[i3] = nums1[i1];
+//                i1++;
+//            }
+//            else {
+//                nums3[i3] = nums2[i2];
+//                i2++;
+//            }
+//            i3++;
+//        }
+//        while (i1 < m){
+//            nums3[i3] = nums1[i1];
+//            i1++;
+//            i3++;
+//        }
+//        while (i2 < n){
+//            nums3[i3] = nums2[i2];
+//            i2++;
+//            i3++;
+//        }
+//        return nums3;
+
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {3,5,7,0,0,0};
+        int[] nums2  = {2,3,6};
+
+        int[] merges = merge2(nums1, 3, nums2, 3);
+        System.out.println("");
+
+    }
 }
--- src/ca/bytetube/_00_leetcode/lc_arraylist_02_SortColors.java
+++ src/ca/bytetube/_00_leetcode/lc_arraylist_02_SortColors.java
@@ -1,5 +1,8 @@
 package ca.bytetube._00_leetcode;
 
+import ca.bytetube._00_leetcode.common.ListNode;
+import ca.bytetube._03_list.List;
+
 /**
  * https://leetcode.com/problems/sort-colors/
  * @author dal
@@ -34,9 +37,109 @@ public class lc_arraylist_02_SortColors {
         }
     }
 
-    private void swap(int[] nums, int i, int j) {
+    public static int[] partitionArray(int[] arr,int pivot){
+        int l = 0;
+        int r = arr.length - 1;
+        int curr = 0;
+
+        while (curr <= r) {
+            if (arr[curr] > pivot) {
+                swap(arr, curr, r);
+                r--;
+            }else if (arr[curr] < pivot) {
+                swap(arr, curr, l);
+                curr++;
+                l++;
+            }else {
+                curr++;
+            }
+        }
+        return arr;
+
+    }
+
+    public static ListNode partitionList(ListNode head, int pivot){
+        //利用单链表的重连特性
+        if (head == null) return head;
+        ListNode lH = null;
+        ListNode lT = null;
+        ListNode eH = null;
+        ListNode eT = null;
+        ListNode mH = null;
+        ListNode mT = null;
+
+        //用来保存下一个待分配的节点
+        ListNode next = null;
+
+        while (head != null){
+            next = head.next;
+
+            head.next = null;
+            if (head.val < pivot) {
+                if (lH == null) {
+                    lH = head;
+                    lT = head;
+                }else {
+                    lT.next = head;
+                    lT = head;
+                }
+            } else  if (head.val == pivot) {
+                if (eH == null) {
+                    eH = head;
+                    eT = head;
+                }else {
+                    eT.next = head;
+                    eT = head;
+                }
+            }else {
+                if (mH == null) {
+                    mH = head;
+                    mT = head;
+                }else {
+                    mT.next = head;
+                    mT = head;
+                }
+            }
+
+            head = next;
+        }
+
+        //less ---> equal ---> more
+        //less ---> equal
+        if (lT != null) {
+            lT.next = eH;
+            eT = eT == null  ? lT: eT;
+        }
+
+        //equal ---> more
+        if (eT != null) {
+            eT.next = mH;
+
+        }
+
+        return lH != null? lH:eH != null ? eH: mH;
+
+    }
+
+    private static void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
+
+    public static void print(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] arr = {10,2,99,7,5,4,6,2,8,10,11,1,-10,87};
+        int[] array = partitionArray(arr, 10);
+        print(array);
+    }
+
+
 }
